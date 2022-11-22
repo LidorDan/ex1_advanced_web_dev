b24d5551d46f141edbd1bc000e43c1ee9287bf69
ofirshtrosberg <ofiri112@gmail.com>
2022-11-22 19:20:40 +0200

    design

--- ex1/src/components/Products.js
+++ ex1/src/components/Products.js
@@ -1,14 +1,16 @@
-import "./Products.css"
+import "./Products.css";
 
-function Products(prod) {
-
-    return (
-        <div className="Products">
-            <div className="Products_name "> {prod.title} </div>
-            <div className="Products_price"> {prod.price} </div>
-            <img src={prod.imgsrc} className="Products_image"/>
-        </div>
-    );
+function Product(prod) {
+  return (
+    <div className="Product">
+      <div className="ProductDetailsDiv">
+        <label className="Product_name "> {prod.title} </label>
+        <br></br>
+        <label className="Product_price">Price: {prod.price} $</label>
+      </div>
+      <img src={prod.imgsrc} className="Product_image" alt="" />
+    </div>
+  );
 }
 
-export default Products;
+export default Product;
