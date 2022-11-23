39f7386dabb6b63ab668909b1a066f1e36e97961
ofirshtrosberg <ofiri112@gmail.com>
2022-11-23 18:20:25 +0200

    description works need to add design

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -4,12 +4,70 @@ import ProductsPage from "./pages/ProductsPage";
 import ProductInfo from "./pages/ProductInfo";
 
 function App() {
+  const products = [
+    {
+      id: "1",
+      title: "Milk",
+      price: 5.0,
+      imgsrc: "./Images/img1.png",
+      description: "Milk 3% Tnuva",
+    },
+    {
+      id: "2",
+      title: "Bread",
+      price: 10.5,
+      imgsrc: "./Images/img2.png",
+      description: "White bread",
+    },
+    {
+      id: "3",
+      title: "Cheese",
+      price: 13.9,
+      imgsrc: "./Images/img3.png",
+      description: "Yellow cheese",
+    },
+    {
+      id: "4",
+      title: "Apple",
+      price: 8.9,
+      imgsrc: "./Images/img4.png",
+      description: "Organic apple",
+    },
+    {
+      id: "5",
+      title: "Tomato",
+      price: 6.5,
+      imgsrc: "./Images/img5.png",
+      description: "Organic tomato",
+    },
+    {
+      id: "6",
+      title: "Avocado",
+      price: 21.9,
+      imgsrc: "./Images/img6.png",
+      description: "Organic avocado",
+    },
+    {
+      id: "7",
+      title: "Eggs",
+      price: 12.0,
+      imgsrc: "./Images/img7.png",
+      description: "description",
+    },
+  ];
   return (
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route exact path="/" element={<ProductsPage />}></Route>
-          <Route path="/ProductInfo/:id" element={<ProductInfo/>}></Route>
+          <Route
+            exact
+            path="/"
+            element={<ProductsPage products={products} />}
+          ></Route>
+          <Route
+            path="/ProductInfo/:id"
+            element={<ProductInfo products={products} />}
+          ></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- ex1/src/pages/ProductInfo.js
+++ ex1/src/pages/ProductInfo.js
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
 import "../styles/ProductInfo.css";
-function ProductInfo() {
+
+function ProductInfo(products) {
   // need to send the item to this function in order to use the description
   let params = useParams();
-  return <label>{params.id}</label>;
+  var item = "";
+  for (let i = 0; i < products.products.length; i++) {
+    if (products.products[i].id === params.id) {
+      item = products.products[i];
+    }
+  }
+  return <label>{item.description}</label>;
 }
 
 export default ProductInfo;
