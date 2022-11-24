e7c249b0085900d9b504c2ccb996414eac1251a2
ofirshtrosberg <ofiri112@gmail.com>
2022-11-24 11:10:15 +0200

    11:10

--- ex1/src/components/Product.js
+++ ex1/src/components/Product.js
@@ -1,18 +1,17 @@
 import "../styles/ProductInfo.css";
 function Product(prod) {
   return (
-   <div className="Product">
-   
+    <div className="Product">
       <div className="ProductDetailsDiv">
         <label className="Product_name "> {prod.title} </label>
         <br></br>
         <label className="Product_price">Price: {prod.price} $</label>
       </div>
-      <a className="LinkToDEscription" href={"/ProductInfo/" + prod.id}>
-        Description</a>
-        <img src={prod.imgsrc1} className="Product_image" alt="" />
+      <a className="LinkToDescription" href={"/ProductInfo/" + prod.id}>
+        Description
+      </a>
+      <img src={prod.imgsrc1} className="Product_image" alt="" />
     </div>
-  
   );
 }
 export default Product;
